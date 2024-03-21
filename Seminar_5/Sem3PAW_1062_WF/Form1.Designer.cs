12f0c211fc204d548db535bcf26ad734c055cae6
CRISTIAN CIUREA <cristianciurea@gmail.com>
2024-03-21 10:44:49 +0200

    update

---
+++ Seminar_5/Sem3PAW_1062_WF/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace Sem3PAW_1062_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCod = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbSex = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbVarsta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.salvareFisierTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.citireDinFisierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cod:";
+            // 
+            // tbCod
+            // 
+            this.tbCod.Location = new System.Drawing.Point(299, 52);
+            this.tbCod.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbCod.Name = "tbCod";
+            this.tbCod.Size = new System.Drawing.Size(132, 22);
+            this.tbCod.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sex:";
+            // 
+            // cbSex
+            // 
+            this.cbSex.FormattingEnabled = true;
+            this.cbSex.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cbSex.Location = new System.Drawing.Point(299, 112);
+            this.cbSex.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbSex.Name = "cbSex";
+            this.cbSex.Size = new System.Drawing.Size(160, 24);
+            this.cbSex.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 176);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Varsta:";
+            // 
+            // tbVarsta
+            // 
+            this.tbVarsta.Location = new System.Drawing.Point(299, 176);
+            this.tbVarsta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbVarsta.Name = "tbVarsta";
+            this.tbVarsta.Size = new System.Drawing.Size(132, 22);
+            this.tbVarsta.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 229);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nume:";
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(299, 229);
+            this.tbNume.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(132, 22);
+            this.tbNume.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 283);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Note:";
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(299, 283);
+            this.tbNote.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(132, 22);
+            this.tbNote.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 370);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 47);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Creare student";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(510, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 47);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Vizualizare date";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salvareFisierTextToolStripMenuItem,
+            this.citireDinFisierToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 33);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // salvareFisierTextToolStripMenuItem
+            // 
+            this.salvareFisierTextToolStripMenuItem.Name = "salvareFisierTextToolStripMenuItem";
+            this.salvareFisierTextToolStripMenuItem.Size = new System.Drawing.Size(160, 29);
+            this.salvareFisierTextToolStripMenuItem.Text = "Salvare fisier text";
+            this.salvareFisierTextToolStripMenuItem.Click += new System.EventHandler(this.salvareFisierTextToolStripMenuItem_Click);
+            // 
+            // citireDinFisierToolStripMenuItem
+            // 
+            this.citireDinFisierToolStripMenuItem.Name = "citireDinFisierToolStripMenuItem";
+            this.citireDinFisierToolStripMenuItem.Size = new System.Drawing.Size(140, 29);
+            this.citireDinFisierToolStripMenuItem.Text = "Citire din fisier";
+            this.citireDinFisierToolStripMenuItem.Click += new System.EventHandler(this.citireDinFisierToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbNume);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbVarsta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbSex);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCod);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCod;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbSex;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbVarsta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salvareFisierTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem citireDinFisierToolStripMenuItem;
+    }
+}
+
---
+++ Seminar_5/Sem3PAW_1062_WF/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sem3PAW_1062_WF
+{
+    public partial class Form1 : Form
+    {
+
+        List<Student> listaStud = new List<Student>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbCod.Text == "")
+                errorProvider1.SetError(tbCod, "Introduceti codul!");
+            else
+                if (cbSex.Text == "")
+                errorProvider1.SetError(cbSex, "Selectati sexul!");
+            else
+                if (tbVarsta.Text == "")
+                errorProvider1.SetError(tbVarsta, "Introduceti varsta!");
+            else
+                if (tbNume.Text == "")
+                errorProvider1.SetError(tbNume, "Introduceti numele!");
+            else
+                if (tbNote.Text == "")
+                errorProvider1.SetError(tbNote, "Introduceti notele!");
+            else
+            {
+                errorProvider1.Clear();
+                try
+                {
+                    int cod = Convert.ToInt32(tbCod.Text);
+                    char sex = Convert.ToChar(cbSex.Text);
+                    int varsta = Convert.ToInt32(tbVarsta.Text);
+                    string nume = tbNume.Text;
+                    string noteS = tbNote.Text;
+                    string[] vectNoteS = noteS.Split(',');
+                    int[] vectNote = new int[vectNoteS.Length];
+                    for (int i = 0; i < vectNoteS.Length; i++)
+                        vectNote[i] = Convert.ToInt32(vectNoteS[i]);
+
+                    Student stud = new Student(cod, sex, varsta, nume, vectNote);
+                    MessageBox.Show(stud.ToString());
+                    listaStud.Add(stud);
+
+                    salvareFisierTextToolStripMenuItem_Click(this, e);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    tbCod.Clear();
+                    cbSex.Text = "";
+                    tbVarsta.Clear();
+                    tbNume.Clear();
+                    tbNote.Clear();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2(listaStud);
+            //frm.Show();
+            frm.ShowDialog();
+        }
+
+        private void salvareFisierTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("fisier.txt");
+            sw.WriteLine(tbCod.Text);
+            sw.WriteLine(cbSex.Text);
+            sw.WriteLine(tbVarsta.Text);
+            sw.WriteLine(tbNume.Text);
+            sw.WriteLine(tbNote.Text);
+            sw.Close();
+        }
+
+        private void citireDinFisierToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("fisier.txt");
+            tbCod.Text = sr.ReadLine();
+            cbSex.Text = sr.ReadLine();
+            tbVarsta.Text = sr.ReadLine();
+            tbNume.Text = sr.ReadLine();
+            tbNote.Text = sr.ReadLine();
+            sr.Close();
+        }
+    }
+}
